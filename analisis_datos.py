2fbe1316c0ea2e104e9658afc287dceb17adca88
Steb Moliech <moliech.ej@gmail.com>
2026-08-07 23:05:41 -0500

    Actividad en clase: Script analisis_datos.py funcional

---
+++ analisis_datos.py
@@ -0,0 +1,8 @@
+# Definición de variables
+temperatura = 40
+ciudad = "Cartago"
+# Condicional if-else
+if temperatura > 35:
+    print("Hace calor en", ciudad)
+else:
+    print("Hace frío en", ciudad)
